5b1314979eb1210a9b3f1668c234bcc18423730c
mohamed <m635379@gmail.com>
2023-10-15 21:18:18 +0200

    add featured

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./app.css";
 import Home from "./pages/home/Home";
 import Hotel from "./pages/hotel/Hotel";
 import Hotels from "./pages/hotels/Hotels";
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,22 @@
+import Featured from "../../components/featured/Featured";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
+import "./home.css";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+        {/* <h1 className="homeTitle">Browse by property type</h1> */}
+        {/* <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
+        <MailList />
+        <Footer /> */}
+      </div>
     </div>
   );
 };
